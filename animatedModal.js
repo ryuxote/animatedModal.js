a8df349c36b6fa89194a6917da84961d24a9a698
rmdwirizki <rmdwirizki@gmail.com>
2016-07-03 23:42:32 +0700

    Add isOpen, close and open modal API

--- animatedModal.js
+++ animatedModal.js
@@ -3,41 +3,41 @@
  * author: João Pereira
  * website: http://www.joaopereira.pt
  * email: joaopereirawd@gmail.com
- * Licensed MIT 
+ * Licensed MIT
 =========================================*/
 
 (function ($) {
- 
+
     $.fn.animatedModal = function(options) {
         var modal = $(this);
-        
+
         //Defaults
         var settings = $.extend({
-            modalTarget:'animatedModal', 
-            position:'fixed', 
-            width:'100%', 
-            height:'100%', 
-            top:'0px', 
-            left:'0px', 
-            zIndexIn: '9999',  
-            zIndexOut: '-9999',  
-            color: '#39BEB9', 
-            opacityIn:'1',  
-            opacityOut:'0', 
+            modalTarget:'animatedModal',
+            position:'fixed',
+            width:'100%',
+            height:'100%',
+            top:'0px',
+            left:'0px',
+            zIndexIn: '9999',
+            zIndexOut: '-9999',
+            color: '#39BEB9',
+            opacityIn:'1',
+            opacityOut:'0',
             animatedIn:'zoomIn',
             animatedOut:'zoomOut',
-            animationDuration:'.6s', 
-            overflow:'auto', 
+            animationDuration:'.6s',
+            overflow:'auto',
             // Callbacks
-            beforeOpen: function() {},           
-            afterOpen: function() {}, 
-            beforeClose: function() {}, 
+            beforeOpen: function() {},
+            afterOpen: function() {},
+            beforeClose: function() {},
             afterClose: function() {}
- 
-            
+
+
 
         }, options);
-        
+
         var closeBt = '.close-'+settings.modalTarget;
 
         //console.log(closeBt)
@@ -69,58 +69,70 @@
         //Apply stles
         id.css(initStyles);
 
-        modal.click(function(event) {       
+        $(document).on('click', modal.selector, function(event) {
             event.preventDefault();
-            $('body, html').css({'overflow':'hidden'});
-            if (href == idConc) {
-                if (id.hasClass(settings.modalTarget+'-off')) {
-                    id.removeClass(settings.animatedOut);
-                    id.removeClass(settings.modalTarget+'-off');
-                    id.addClass(settings.modalTarget+'-on');
-                } 
-
-                 if (id.hasClass(settings.modalTarget+'-on')) {
-                    settings.beforeOpen();
-                    id.css({'opacity':settings.opacityIn,'z-index':settings.zIndexIn});
-                    id.addClass(settings.animatedIn);  
-                    id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterOpen);
-                };  
-            } 
+            open();
         });
-
-
 
         $(document).on('click', closeBt, function(event) {
             event.preventDefault();
+            close();
+        });
+
+        function open () {
+          $('body, html').css({'overflow':'hidden'});
+          if (href == idConc) {
+              if (id.hasClass(settings.modalTarget+'-off')) {
+                  id.removeClass(settings.animatedOut);
+                  id.removeClass(settings.modalTarget+'-off');
+                  id.addClass(settings.modalTarget+'-on');
+              }
+
+               if (id.hasClass(settings.modalTarget+'-on')) {
+                  settings.beforeOpen();
+                  id.css({'opacity':settings.opacityIn,'z-index':settings.zIndexIn});
+                  id.addClass(settings.animatedIn);
+                  id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterOpen);
+              };
+          }
+        }
+
+        function close () {
             $('body, html').css({'overflow':'auto'});
 
             settings.beforeClose(); //beforeClose
             if (id.hasClass(settings.modalTarget+'-on')) {
                 id.removeClass(settings.modalTarget+'-on');
                 id.addClass(settings.modalTarget+'-off');
-            } 
+            }
 
             if (id.hasClass(settings.modalTarget+'-off')) {
                 id.removeClass(settings.animatedIn);
                 id.addClass(settings.animatedOut);
                 id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterClose);
             };
+        }
 
-        });
+        function isOpen () {
+            return id.hasClass(settings.modalTarget+'-on');
+        }
 
-        function afterClose () {       
+        function afterClose () {
+            id.off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
             id.css({'z-index':settings.zIndexOut});
             settings.afterClose(); //afterClose
         }
 
-        function afterOpen () {       
+        function afterOpen () {
             settings.afterOpen(); //afterOpen
         }
+
+        return {
+            isOpen: isOpen,
+            open: open,
+            close: close
+        };
 
     }; // End animatedModal.js
 
 }(jQuery));
-
-
-
-        
